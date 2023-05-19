0a52513a1a2ca785b7bc13f9af09834f69658005
Abhishek <abhisheklitlost@gmail.com>
2023-05-19 15:48:22 +0530

    level 4: update app.js by uncommenting bcrypt

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -39,47 +40,48 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+
 // ************ using bcrypt ************//
 
-// app.post("/register", async function(req, res) {
-//     bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
-//         try {
-//             const newUser = new User ({
-//                 email: req.body.username,
-//                 password: hash
-//             });
-//             await newUser.save();
-//             res.render("secrets");
-//         }
-//         catch (err) {
-//             console.log(err);
-//         }
-//     });
-// });
+app.post("/register", async function(req, res) {
+    bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+        try {
+            const newUser = new User ({
+                email: req.body.username,
+                password: hash
+            });
+            await newUser.save();
+            res.render("secrets");
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+});
 
-// app.post("/login", async function(req, res) {
-//     try {
-//         const username = req.body.username;
-//         const password = req.body.password;
-//         await User.findOne({email: username}).then(function (foundUser) {
-//             if (foundUser) {
-//                 bcrypt.compare(password, foundUser.password, function(err, result) {
-//                     if (result === true) {
-//                         res.render("secrets");
-//                     } else {
-//                         res.send("Wrong password! Please enter the right password");
-//                     }
-//                 })
+app.post("/login", async function(req, res) {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        await User.findOne({email: username}).then(function (foundUser) {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        res.send("Wrong password! Please enter the right password");
+                    }
+                })
                 
-//             } else {
-//                 res.send("No user found!");
-//             }
-//         })
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// });
+            } else {
+                res.send("No user found!");
+            }
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
 
 // ************ end of bcrypt ************//
 
