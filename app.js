6cf317b46a78d3e89451adf334ad38d5807c2378
Abhishek <abhisheklitlost@gmail.com>
2023-05-19 10:53:47 +0530

    Add env file to store API Keys

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -18,8 +19,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "Thisismylittlesecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
